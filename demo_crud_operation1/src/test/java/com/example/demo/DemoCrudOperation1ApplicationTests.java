fc0f731d69bc632bd13775917ea8ba026bca4697
kirangond <kirangond05@gmail.com>
2024-01-21 17:55:11 +0530

    insert, update, delete and retrive

--- demo_crud_operation1/src/test/java/com/example/demo/DemoCrudOperation1ApplicationTests.java
+++ demo_crud_operation1/src/test/java/com/example/demo/DemoCrudOperation1ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,21 +9,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.demo.crudop.entity.Student;
 import com.demp.crudo.repositories.StudentRepository;
 
+
 @SpringBootTest
 class DemoCrudOperation1ApplicationTests {
 	
-	@Autowired //it will create object of the class 
-	StudentRepository studentRepo;
+	//it will create object of the class 
+	
+	@Autowired
+	StudentRepository studentrepo;
+	
+	@Test
+	public void dummy()
+	{
+		System.out.println("dummy excuted: ");
+	}
 
 	@Test
-	void insertStudentRecords() {
+	public void insertStudentRecords() {
 	//we should not create object in this avoid it.	
 		Student student = new Student();
 		student.setSname("pavan");
 		student.setScourse("spring");
 		student.setSfee(20000);
-		studentRepo.save(student);
+		studentrepo.save(student);
+		
+	
 		
 	}
+	
 
 }
